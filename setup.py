365a2f727a3baac2bdf384d81ba12731ab8a9de6
Fanilo Andrianasolo <andfanilo@gmail.com>
2021-12-01 21:35:15 +0100

    Bump to version 0.0.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme() -> str:
 
 setuptools.setup(
     name="streamlit-lottie",
-    version="0.0.3",
+    version="0.0.4",
     author="Fanilo ANDRIANASOLO",
     author_email="andfanilo@gmail.com",
     description="A Streamlit custom component to load Lottie animations",
